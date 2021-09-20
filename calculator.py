a427991566269e2dd097192f00040d68933220ae
IlyaStepanov11 <90521787+IlyaStepanov11@users.noreply.github.com>
2021-09-20 22:02:35 +0300

    Add files via upload
    Создан калькулятор.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+a = float(input())
+math_operation = input()
+b = float(input())
+
+if math_operation == "+":
+    result = a + b
+elif math_operation == "-":
+    result = a - b
+elif math_operaton == "*":
+    result = a * b
+elif math_operation == "/":
+    result = a / b
+elif math_operation == "^":
+    result = a ^ b
+
+print(result)
